6f84eb29b8074f74ead0615be2754df4c71790a1
Nick Scialli <7538045+nas5w@users.noreply.github.com>
2019-02-22 19:51:10 -0500

    Handle params

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = router();
 const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/test-route/:val', (req, res) => res.send(res.params.val));
 
 app
   .listen(port)
