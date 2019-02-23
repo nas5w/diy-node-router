eec74eaadd55acb320636058fbcc0911baec264f
Nick Scialli <7538045+nas5w@users.noreply.github.com>
2019-02-22 20:22:39 -0500

    Convert to revealing module

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = router();
 const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/test-route/:thing', (req, res) => res.send(req.params.thing));
 
 app
   .listen(port)
